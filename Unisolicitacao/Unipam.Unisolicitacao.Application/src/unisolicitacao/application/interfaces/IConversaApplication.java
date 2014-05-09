09f18725153b65897f8b4d524c9732e1e8e70bc1
petrovick <petrovickg@github.com>
2014-05-09 16:21:51 -0300

    application

---
+++ Unisolicitacao/Unipam.Unisolicitacao.Application/src/unisolicitacao/application/interfaces/IConversaApplication.java
@@ -0,0 +1,11 @@
+package unisolicitacao.application.interfaces;
+
+import java.util.Set;
+
+import unisolicitacao.application.generic.IGenericApplication;
+import unisolicitacao.business.Conversa;
+
+public interface IConversaApplication extends IGenericApplication<Conversa, Integer>
+{
+	
+}
